cd936e3416ef3f8eb8fd72ad7a0fa9310dc51aac
unknown <rajeshpalai55@gmail.com>
2021-10-17 14:08:43 +0530

    main: initial commit.

---
+++ src/main/java/in/rajesh/details/Student.java
@@ -0,0 +1,33 @@
+package in.rajesh.details;
+public class Student {
+	private int sid;
+	private String sname;
+	private double sfee;
+	public Student() {
+		super();
+	}
+    public int getSid() {
+		return sid;
+	}
+   public void setSid(int sid) {
+		this.sid = sid;
+	}
+   public String getSname() {
+		return sname;
+	}
+   public void setSname(String sname) {
+		this.sname = sname;
+	}
+   public double getSfee() {
+		return sfee;
+	}
+   public void setSfee(double sfee) {
+		this.sfee = sfee;
+	}
+   @Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", sfee=" + sfee + "]";
+	}
+	
+
+}
